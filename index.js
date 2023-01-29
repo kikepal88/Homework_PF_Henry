badb3633cb05872f7358f801a3e0ad29d430ed67
RominaMB <romimolinabravo@gmail.com>
2023-01-29 13:57:43 -0300

    Se creo la función romina

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function laCajaDePandora(numero){
   return (`Está opción es la de Romina ${numero}`);
 }
+
+function romina(){
+  return ({
+    nombre: 'Romina',
+    edad: 27,
+    nacionalidad: 'Argentina',
+  })
+}
