69b509e9cfdb4bbf022df31feb136746f2ca4abb
rsmalberca <renzo.smalberca@gmail.com>
2023-01-29 11:57:51 -0500

    dataCreator function added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function laCajaDePandora(numero) {
   return `Está opción es la de Renzo ${numero}`;
 }
+
+const dataCreator = () => {
+  return {
+    name: "Renzo San Martin",
+    age: 29,
+    nationality: "Peruvian",
+  };
+};
