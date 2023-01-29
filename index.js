28a55f05dd0d12b8a962fb90be03af7a2186a75f
Kikepal88 <omarpal88@gmail.com>
2023-01-29 11:57:25 -0500

    Se creao la función Omar

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function laCajaDePandora(numero){
   return (`Está opción es la de Omar ${numero}`);
 }
+
+function omar() {
+  return({
+    nombre: "Omar",
+    edad: 34,
+    nacionalidad: "Colombiano",
+  })
+}
